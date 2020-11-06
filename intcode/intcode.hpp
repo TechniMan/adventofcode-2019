71a4ca6147bd990861a1070a8249d5bb837bfd4a
Will Thomas <technimanx@googlemail.com>
2020-11-06 22:01:12 +0000

    Saving progress - I haven't touched this in 11 months so I don't know where it is but also don't want to lose it

--- intcode/intcode.hpp
+++ intcode/intcode.hpp
@@ -7,11 +7,38 @@ void setvalueat(uint index, int val) { memory[index] = std::to_string(val); }
 int runprogram() {
 	bool shouldEscape = false;
 	int opcode = 0;
-	for (uint progCounter = 0U; !shouldEscape;)
-	{
-		opcode = getvalueat(progCounter);
-		switch (opcode)
-		{
+	for (uint progCounter = 0U; !shouldEscape;) {
+        if (progCounter >= memory.size()) {
+            shouldEscape = true;
+            std::cout << "PROGRAM OVERFLOW ENCOUNTERED" << std::endl;
+            break;
+        }
+        
+        std::vector<ushort> paramModes = { 0U, 0U, 0U };
+        std::string instruction = std::to_string(getvalueat(progCounter));
+        // 2-digit opcode
+		if (instruction.size() < 3) {
+            opcode = std::stoi(instruction);
+        }
+        // 3 digits param modes
+        else if (instruction.size() < 6) {
+            opcode = std::stoi(instruction.substr(instruction.size() - 2));
+            paramModes[2] = std::stoi(instruction.substr(0, 1));
+            if (instruction.size() > 3) {
+                paramModes[1] = std::stoi(instruction.substr(1, 1));
+            }
+            if (instruction.size() > 4) {
+                paramModes[0] = std::stoi(instruction.substr(2, 1));
+            }
+        }
+        else {
+            std::cout << "ERRONEOUS PROGRAM INSTRUCTION" << std::endl;
+            break;
+        }
+
+        std::string inp;
+        int o;
+		switch (opcode) {
 		//add
 		case 1:
 			setvalueat(getvalueat(progCounter + 3),
@@ -27,6 +54,20 @@ int runprogram() {
 				getvalueat(getvalueat(progCounter + 2)));
 			progCounter += 4;
 			break;
+        
+        //inp
+        case 3:
+            std::cout << "INPUT REQUIRED: ";
+            std::cin >> inp;
+            int i = std::stoi(inp);
+            setvalueat(progCounter + 1, i);
+            break;
+        
+        //out
+        case 4:
+            o = getvalueat(getvalueat(progCounter + 1));
+            std::cout << "OUTPUT: " << std::to_string(o) << std::endl;
+            break;
 
 		//esc
 		case 99:
